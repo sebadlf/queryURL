4116316906125e6421faf93bb1a15b83ae013d9b
nicov <n.villalonga@globant.com>
2015-10-23 09:10:06 -0300

    get environment from constants file

--- src/app/app-resource.js
+++ src/app/app-resource.js
@@ -13,20 +13,40 @@ angular.module('getResource', [
  * @param  {function} $timeout  AngularJS wrapper for window.setTimeout.
  * @return {object}   $resource API call results.
  */
-.factory('getResource', ['$resource', '$q', '$timeout', function($resource, $q, $timeout) {
-	return $resource(
-		// BlueLevel Service (DEV environment)
-		//'http://emc.dev.bluelevel.co.uk/Services/ServiceProviders.asmx/:resource/',
-		// EMC Service Production
-		'https://cspdb.emc.com/Services/ServiceProviders.asmx/:resource/',
-		// EMC Service Development
-		//'http://cspappdev01//Services/ServiceProviders.asmx/:resource/',
-		{
-			callback: 'JSON_CALLBACK'
-		},
-		{
-			get:    {method: 'JSONP'},
-			params: {serviceProviderId: '@serviceProviderId'}
-		}
-	);
+.factory('getResource', ['$resource', '$q', '$timeout', 'configuration', function($resource, $q, $timeout, configuration) {
+	
+	var factory = {};
+
+	factory.setEnvironment = function() {
+		var url;
+
+		(function getUrl() {
+			url = configuration.environment.filter(function(e) {
+				//this is now hardcoded with production, later it will be parameter from getUrl(env)
+				return e.environment === 'production';
+			});
+
+			url = url[0].url;
+			console.log(url);
+		})();
+
+		return $resource(
+			// BlueLevel Service (DEV environment)
+			//'http://emc.dev.bluelevel.co.uk/Services/ServiceProviders.asmx/:resource/',
+			// EMC Service Production
+			url,
+			// EMC Service Development
+			//'http://cspappdev01//Services/ServiceProviders.asmx/:resource/',
+			{
+				callback: 'JSON_CALLBACK'
+			},
+			{
+				get:    {method: 'JSONP'},
+				params: {serviceProviderId: '@serviceProviderId'}
+			}
+		);
+	};
+		
+
+	return factory;
 }]);
